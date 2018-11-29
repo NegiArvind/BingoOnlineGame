44b0bb7841629302da4d5a3051741cf52842cc66
arvind <negiarvind229@gmail.com>
2018-11-29 13:43:39 +0530

    final

--- app/src/main/java/arvindandroid/com/arvind/bingoonlinegame/Activities/LoginActivity.java
+++ app/src/main/java/arvindandroid/com/arvind/bingoonlinegame/Activities/LoginActivity.java
@@ -4,12 +4,16 @@ import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -41,6 +45,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 import arvindandroid.com.arvind.bingoonlinegame.Common;
@@ -72,6 +78,21 @@ public class LoginActivity extends AppCompatActivity {
         firebaseDatabase=FirebaseDatabase.getInstance();
         usersReference=firebaseDatabase.getReference();
         getSupportActionBar().setTitle("Bingo Online");
+//
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.packagename",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
 
 
 
